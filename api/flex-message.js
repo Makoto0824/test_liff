7fa116cdfc1a2a1d50f19bfc7f4cf4f9037bb6d4
Makoto Hirasawa <m.hirasawa0824@gmail.com>
2025-08-04 16:05:56 +0900

    🎫 Flex Message最終調整 - 御朱印、10%OFF、ボタンテキスト最適化

--- api/flex-message.js
+++ api/flex-message.js
@@ -14,7 +14,7 @@ module.exports = {
         "type": "image",
         "url": "https://test-liff-nu.vercel.app/images/cat.png",
         "size": "full",
-        "aspectRatio": "20:13",
+        "aspectRatio": "16:9",
         "aspectMode": "cover",
         "margin": "md"
       },
@@ -22,7 +22,7 @@ module.exports = {
         "type": "image",
         "url": "https://test-liff-nu.vercel.app/images/ticket.png",
         "size": "full",
-        "aspectRatio": "20:13",
+        "aspectRatio": "1:1",
         "aspectMode": "cover",
         "margin": "md"
       },
