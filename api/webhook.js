4423ab0c5ae90f16c7bd9f2df4615af2d0e83ece
Makoto Hirasawa <m.hirasawa0824@gmail.com>
2025-08-27 10:11:34 +0900

    🖼️ 画像をcatrip_50_2.jpgに差し替え、Version 2.4.0

--- api/webhook.js
+++ api/webhook.js
@@ -117,7 +117,7 @@ async function handlePostback(event, accessToken) {
                         "contents": [
                             {
                                 "type": "image",
-                                "url": "https://test-liff-nu.vercel.app/images/catrip_50.jpg",
+                                "url": "https://test-liff-nu.vercel.app/images/catrip_50_2.jpg",
                                 "size": "full",
                                 "aspectRatio": "128:381",
                                 "aspectMode": "cover"
@@ -266,7 +266,7 @@ async function handleMessage(event, accessToken) {
                         "contents": [
                             {
                                 "type": "image",
-                                "url": "https://test-liff-nu.vercel.app/images/catrip_50.jpg",
+                                "url": "https://test-liff-nu.vercel.app/images/catrip_50_2.jpg",
                                 "size": "full",
                                 "aspectRatio": "128:381",
                                 "aspectMode": "cover"
